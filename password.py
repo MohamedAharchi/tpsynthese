0775b75d5b61104cb59cfc68c75ac8f37f18e6ec
Mohamed <aharchi.mohamed@gmail.com>
2016-01-06 15:49:29 +0100

    Question 2

--- password.py
+++ password.py
@@ -12,20 +12,33 @@ def getNext(password):
     pwd = list(password)  #1 Le mot de passe en paramètre est affecté dans une liste.
     found = False
     i=len(pwd)-1 # i = dernière lettre du mot de passe.
-    
-    while not found:
-        if pwd[i] < 'z':
-           pwd[i] = chr(ord(pwd[i])+1)  #2 Incrémentation de 1 sur le code ASCII de la dernière lettre.
-           # ord() récupère le code ASCII du paramètre 
-           # chr() traduit le code ASCII en lettre.           
-           found = True             
-        else:            
-           pwd[i] = 'a'
-           i = i-1
-           pwd[i] = chr(ord(pwd[i])+1)
-           found = True  
-    
-    return ''.join(pwd) #3 On retourne le nouveau mot de passe.
+    testZonly = True
+    """
+    Série de tests pour 'zzzz'
+    >>> getNext('zzzzz')
+    """
+    for i in range(0, len(password)):
+        if password[i] != 'z':
+           testZonly = False
+           break
+        else :
+            testZonly = True
+            i = i + 1
+    if testZonly:
+        raise Exception('ERREUR !')
+    else:
+        while not found:
+            if pwd[i] < 'z':
+                pwd[i] = chr(ord(pwd[i])+1)  #2 Incrémentation de 1 sur le code ASCII de la dernière lettre.
+                # ord() récupère le code ASCII du paramètre 
+                # chr() traduit le code ASCII en lettre.           
+                found = True             
+            else:            
+                pwd[i] = 'a'
+                i = i-1
+                pwd[i] = chr(ord(pwd[i])+1)
+                found = True        
+        return ''.join(pwd) #3 On retourne le nouveau mot de passe.
     # Remarque : join() accepte n’importe quel type de valeur.
 
 
--- test_pass.py
+++ test_pass.py
@@ -10,6 +10,9 @@ class TestPassword(unittest.TestCase):
     def test_getNextEndLine(self):
         self.assertEqual(pwd.getNext("abhz"), "abia")
 
+    def test_getNextLenght(self):
+        password = "aaaaa"
+        self.assertEqual(len(pwd.getNext(password), len(password))
 
 # Permet d'exécuter les tests si ce fichier est exécuté
 unittest.main()
